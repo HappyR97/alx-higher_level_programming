a3373c0c48f36ec3d631d7ef649c3853b88d25bf
HappyR97 <ridaberkaoui1997@gmail.com>
2024-01-24 11:40:36 +0100

    Solve task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    biggest = 0
+    for key in a_dictionary:
+        if a_dictionary[key] > biggest:
+            biggest = a_dictionary[key]
+            biggest_key = key
+    return biggest_key
