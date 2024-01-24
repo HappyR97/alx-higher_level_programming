80ddc37f2e33f3b296c26e09a7b3f5212e802521
HappyR97 <ridaberkaoui1997@gmail.com>
2024-01-24 13:06:18 +0100

    Improve task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,11 +7,23 @@ def roman_to_int(roman_string):
                     "C": 100, "D": 500, "M": 1000}
     total = 0
     for i in range(len(roman_string)):
+        if roman_string[i] == "V" and roman_string[i - 1] == 'I':
+            total += 3
+            continue
         if roman_string[i] == "X" and roman_string[i - 1] == 'I':
             total += 8
             continue
-        if roman_string[i] == "V" and roman_string[i - 1] == 'I':
-            total += 3
+        if roman_string[i] == "L" and roman_string[i - 1] == "X":
+            total += 30
+            continue
+        if roman_string[i] == "C" and roman_string[i - 1] == "X":
+            total += 80
+            continue
+        if roman_string[i] == "D" and roman_string[i - 1] == "C":
+            total += 300
+            continue
+        if roman_string[i] == "M" and roman_string[i  - 1] == "C":
+            total += 800
             continue
         total += roman_values[roman_string[i]]
     return total
