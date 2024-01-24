e4288c5124487420a1f2734d13627e11715ca153
HappyR97 <ridaberkaoui1997@gmail.com>
2024-01-24 18:23:29 +0100

    Solve task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list or my_list == None:
+        return 0
+    score = 0
+    weight = 0
+    for element in my_list:
+        score += (element[0] * element[1])
+        weight += element[1]
+    return score / weight
