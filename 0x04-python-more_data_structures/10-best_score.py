619cc58d93b34aa0cf824d4221d5510707d4a0c7
HappyR97 <ridaberkaoui1997@gmail.com>
2024-01-24 11:46:31 +0100

    Hot fix task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,11 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+
     biggest = 0
     for key in a_dictionary:
+        if a_dictionary[key] is None:
+            return None
         if a_dictionary[key] > biggest:
             biggest = a_dictionary[key]
             biggest_key = key
