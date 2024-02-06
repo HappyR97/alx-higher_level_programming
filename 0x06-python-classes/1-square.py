d7060d54c5059a2a8361996022b036110555926a
HappyR97 <ridaberkaoui1997@gmail.com>
2024-02-06 12:35:23 +0100

    Solve task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module defines a class Square"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size):
+        """Initialize the data"""
+        self.__size = size
