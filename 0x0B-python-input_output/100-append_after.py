bcc0bf730635d16089caee0b1f81570c8fc58b93
HappyR97 <ridaberkaoui1997@gmail.com>
2024-02-20 19:02:03 +0100

    Solve task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+
+This module defines a function that inserts a line of text to a file,
+after each line containing a specific string
+
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line to file if specific string is found"""
+    with open(filename, "r+", encoding="utf-8") as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            if search_string in lines[i]:
+                lines.insert(i + 1, new_string)
+                i += 1
+        file.seek(0)
+        file.writelines(lines)
