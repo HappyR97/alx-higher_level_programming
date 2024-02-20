975aa0c23d07b6da1efb263c0c4c4b8a4b3c0ad2
HappyR97 <ridaberkaoui1997@gmail.com>
2024-02-20 13:42:19 +0100

    Solve task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+
+This module defines Student class
+
+"""
+
+
+class Student:
+    """Defines Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Inilializes values"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation"""
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items()
+                if key in attrs}
