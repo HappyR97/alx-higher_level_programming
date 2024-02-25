a98e73f4d91e2346c88b5517d14ffe8433a874f7
HappyR97 <ridaberkaoui1997@gmail.com>
2024-02-25 17:39:50 +0100

    Solve task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width"""
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -43,7 +43,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height"""
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -57,7 +57,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for x"""
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -71,7 +71,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for y"""
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -80,3 +80,10 @@ class Rectangle(Base):
     def area(self):
         """Returns area of rectangle"""
         return self.__height * self.__width
+
+    def display(self):
+        """Prints Rectangle with character #"""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
