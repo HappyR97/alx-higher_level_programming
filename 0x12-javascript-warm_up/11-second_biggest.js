c98bc5040eb54cbb25707ec87d1c1996c82b088e
HappyR97 <ridaberkaoui1997@gmail.com>
2024-04-22 19:06:53 +0100

    Hot fix 2 for task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,6 +6,6 @@ const numbers = args.map(Number);
 if (isNaN(args[2]) || args.length === 1) {
   console.log(0);
 } else {
-  const sorted = numbers.sort((a, b) => a - b);
-  console.log(sorted[sorted.length - 2]);
+  const sorted = numbers.sort((a, b) => b - a);
+  console.log(sorted[1]);
 }
