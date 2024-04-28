206ea03bdea27d5655484bd16b46443a959f3009
Rida Berkaoui <113365863+HappyR97@users.noreply.github.com>
2024-04-28 16:06:40 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const args = process.argv;
-
-function add (a, b) {
-  return a + b;
-}
-
-console.log(add(Number(args[2]), Number(args[3])));
--- 0x12-javascript-warm_up/README.md
+++
@@ -1,1 +0,0 @@
-This is the directory for the "0x12. JavaScript - Warm up" project in ALX SE
